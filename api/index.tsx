291b21ea18723b94d1ce5ed1018f8a35260de454
Sebastiaan Wiechers <sebastiaanwiechers@icloud.com>
2024-04-08 20:18:26 +0200

    added art

--- api/index.tsx
+++ api/index.tsx
@@ -2,23 +2,110 @@ import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 // import { neynar } from 'frog/hubs'
+
 import { handle } from 'frog/vercel'
-import { createCanvas } from 'canvas'
-import fs from 'fs'
+import { createCanvas, CanvasRenderingContext2D } from 'canvas'
+import { put } from "@vercel/blob";
+import { v4 } from 'uuid'
+import { colors } from '../lib/colors.js'
+
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Uncomment to use Edge Runtime.
 // export const config = {
 //   runtime: 'edge',
 // }
 
-const width = 200;
-const height = 200;
+
+function pickColor() {
+  const paletteIndex = Math.floor(Math.random() * colors.length);
+  return colors[paletteIndex];
+}
+
+function randomShapes(
+  ctx: CanvasRenderingContext2D, 
+  palette: string[], 
+  width: number, 
+  height: number) {
+  
+  for (let i = 0; i < 20; i++) {
+
+    ctx.beginPath();
+    ctx.moveTo(width / 2, height / 2); // Start from the center
+
+    const shape = Math.floor(Math.random() * 3);
+    const color = palette[Math.floor(Math.random() * 5)]
+    
+    ctx.fillStyle = color;
+  
+    switch (shape) {
+      case 0: // Draw a rectangle
+        const rx = Math.random() * width;
+        const ry = Math.random() * height;
+        const rw = Math.random() * width / 5;
+        const rh = Math.random() * height / 5;
+        ctx.fillRect(rx, ry, rw, rh);
+        break;
+      case 1: // Draw a circle
+        const cx = Math.random() * width;
+        const cy = Math.random() * height;
+        const r = Math.random() * width / 10;
+        ctx.beginPath();
+        ctx.arc(cx, cy, r, 0, Math.PI * 2, true);
+        ctx.fill();
+        break;
+      case 2: // Draw a line
+        ctx.beginPath();
+        ctx.moveTo(width / 2, height / 2); // Start from the center
+        
+        ctx.lineWidth = Math.random() * 5 + 1; // Random line width between 1 and 6
+
+        const x = Math.random() * width;
+        const y = Math.random() * height;
+     
+          const cp1x = (width / 2) + Math.random() * width;
+          const cp1y = (height / 2) + Math.random() * height;
+          const cp2x = (width / 2) + Math.random() * width;
+          const cp2y = (height / 2) + Math.random() * height;
+          ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+        
+          ctx.stroke();
+
+        ctx.lineWidth = 1;
+
+        
+        break;
+    }
+  }
+}
+
+async function create() {
+  // pick a random color palette
+
+  const palette = pickColor();
+
+
+
+const width = 764;
+const height = 400;
+
 const canvas = createCanvas(width, height) 
 const context = canvas.getContext("2d");
-context.fillStyle = "yellow";
+context.fillStyle = palette[4];
 context.fillRect(0, 0, width, height);
+
+randomShapes(context, palette, width, height);
+
 const buffer = canvas.toBuffer("image/png");
 
+const id = v4()
+const { url } = await put('images/' + id + '.png', buffer, { 
+  access: 'public',
+});
+return url
+}
 
 
 export const app = new Frog({
@@ -28,18 +115,17 @@ export const app = new Frog({
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
 
-app.frame('/', (c) => {
+app.frame('/', async (c) => {
   const { buttonValue, inputText, status } = c
   const fruit = inputText || buttonValue
+  const url = await create()
+
   return c.res({
     image: (
       <div
         style={{
           alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
+          background: `url(${url})`,
           backgroundSize: '100% 100%',
           display: 'flex',
           flexDirection: 'column',
@@ -52,9 +138,10 @@ app.frame('/', (c) => {
       >
         <div
           style={{
-            color: 'white',
+            color: 'black',
             fontSize: 60,
-            fontStyle: 'normal',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            fontStyle: 'bold',
             letterSpacing: '-0.025em',
             lineHeight: 1.4,
             marginTop: 30,
@@ -63,17 +150,12 @@ app.frame('/', (c) => {
           }}
         >
           {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+            ? `Thanks for clooonking 🎩` : ''}
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button value="bananas">clink clonk click me 🧙‍♂️</Button>,
     ],
   })
 })
